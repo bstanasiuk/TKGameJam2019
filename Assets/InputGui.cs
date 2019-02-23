3a9caa9a8dbd5561f38036321a1421ed6ba75024
Maciek <maciek.sozanski@gmail.com>
2019-02-23 13:56:22 +0100

    Add TextMeshPro and Mod MatchController

---
+++ Assets/InputGui.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InputGui : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI firstPlayerText;
+    [SerializeField]
+    TextMeshProUGUI secondPlayerText;
+
+
+    public void HideBoth()
+    {
+        HideFirstPlayerText();
+        HideSecondPlayerText();
+    }
+
+    public void HideFirstPlayerText()
+    {
+        firstPlayerText.gameObject.SetActive(false);
+    }
+
+    public void HideSecondPlayerText()
+    {
+        secondPlayerText.gameObject.SetActive(false);
+    }
+
+    public void ShowFirstPlayerText()
+    {
+        firstPlayerText.gameObject.SetActive(true);
+    }
+
+    public void ShowSecondPlayerText()
+    {
+        secondPlayerText.gameObject.SetActive(true);
+    }
+}
