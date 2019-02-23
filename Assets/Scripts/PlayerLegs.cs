4def3b84c0e142583efd77acb285f390a4116bb5
bstanasiuk <bartlomiej.stanasiuk@gmail.com>
2019-02-23 11:17:31 +0100

    Add leg check aligmnent

--- Assets/Scripts/PlayerLegs.cs
+++ Assets/Scripts/PlayerLegs.cs
@@ -6,9 +6,18 @@ public class PlayerLegs : MonoBehaviour
 {
     [SerializeField] private LegLine _leftLegLine;
     [SerializeField] private LegLine _rightLegLine;
-
-    private void Awake()
+    
+    private void Update()
     {
-        
+        CheckLegAlignment();
+    }
+
+    private void CheckLegAlignment()
+    {
+        var legRange = _rightLegLine.SelectedKeyIndex - _leftLegLine.SelectedKeyIndex;
+        if (legRange >= -2 && legRange <= 1)
+        {
+            print("OK");
+        }
     }
 }
