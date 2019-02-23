b47609e029fe5c6d4bc99f9e8e71350f81f949f5
bstanasiuk <bartlomiej.stanasiuk@gmail.com>
2019-02-23 22:06:36 +0100

    Add key move tip

--- Assets/Scripts/LegLine.cs
+++ Assets/Scripts/LegLine.cs
@@ -3,6 +3,7 @@
 public class LegLine : MonoBehaviour
 {
     [SerializeField] private KeyCode[] _keys;
+    [SerializeField] private LegLine _secondLegLine;
     public LineVisualizerWithMeshKeys LineVisualizer { get; private set; }
     public int? SelectedKeyIndex { get; private set; }
     public int LastSelectedKey { get; private set; }
@@ -20,6 +21,21 @@ public class LegLine : MonoBehaviour
     private void Update()
     {
         HandleInput();
+        ActivateKeys();
+        
+    }
+
+    private void ActivateKeys()
+    {
+        for (var i = 0; i < _keys.Length; i++)
+        {
+            if (IsEligible(i))
+            {
+                LineVisualizer.ShowEligibleFrame(i);
+            }
+            if (SelectedKeyIndex.HasValue && SelectedKeyIndex.Value == i)
+                LineVisualizer.ActivateKey(i);
+        }
     }
 
     private void HandleInput()
@@ -32,11 +48,10 @@ public class LegLine : MonoBehaviour
         else
         {
             for (var i = 0; i < _keys.Length; i++)
-                if (Input.GetKey(_keys[i]) && !SelectedKeyIndex.HasValue)
+                if (Input.GetKey(_keys[i]) && !SelectedKeyIndex.HasValue && IsEligible(i))
                 {
                     SelectedKeyIndex = i;
                     LastSelectedKey = i;
-                    LineVisualizer.ActivateKey(i);
                 }
                 else
                 {
@@ -45,8 +60,10 @@ public class LegLine : MonoBehaviour
         }
     }
 
-/*    private bool IsEligable(int keyIndex)
+    private bool IsEligible(int keyIndex)
     {
-        
-    }*/
+        var lowerLastLinesIndex = Mathf.Min(LastSelectedKey, _secondLegLine.LastSelectedKey);
+        var higherLastLinesIndex = Mathf.Max(LastSelectedKey, _secondLegLine.LastSelectedKey);
+        return keyIndex <= lowerLastLinesIndex + 2 && keyIndex >= higherLastLinesIndex - 2;
+    }
 }
