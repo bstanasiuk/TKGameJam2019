41ba8ca727e21f05468eb2431e028ecdff6a22b0
Maciek <maciek.sozanski@gmail.com>
2019-02-24 02:38:27 +0100

    added ScoreController

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreController : MonoBehaviour
+{
+    [SerializeField]
+    TextMeshProUGUI myScoreText;
+    [SerializeField]
+    bool resetPlayerPrefs;
+
+    const string hashKeyPrefix = "player";
+    string hashKey;
+
+
+    void Start()
+    {
+        if (resetPlayerPrefs) PlayerPrefs.DeleteAll();
+        EventManager.Instance.PlayerDead.AddListener(OnDead);
+        hashKey = hashKeyPrefix + gameObject.layer;
+        if(PlayerPrefs.HasKey(hashKey))
+        {
+            myScoreText.text = PlayerPrefs.GetInt(hashKey).ToString();
+        }
+        else
+        {
+            PlayerPrefs.SetInt(hashKey, 0);
+            myScoreText.text = "0";
+        }
+    }
+
+   
+
+    // Update is called once per frame
+    void OnDead(Vector3 pos)
+    {
+        PlayerPrefs.SetInt(hashKey, PlayerPrefs.GetInt(hashKey) + 1);
+    }
+}
