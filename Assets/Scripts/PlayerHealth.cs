4df5830765a7d089f2273fe837520b6cbb5ee098
bstanasiuk <bartlomiej.stanasiuk@gmail.com>
2019-02-24 11:54:47 +0100

    Fix time

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -67,7 +67,7 @@ public class PlayerHealth : MonoBehaviour
 
     IEnumerator InvincibleTime()
     {
-        yield return new WaitForSeconds(.25f);
+        yield return new WaitForSeconds(1f);
         isInvincible = false;
     }
 }
