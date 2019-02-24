f5dc71b54145b3ec0aa2501fe96f6447c54753ff
PRIDEMASTER <michal.winkler.22.10@gmail.com>
2019-02-24 11:45:35 +0100

    Destroy Armor

---
+++ Assets/Scripts/ArmorParticleDestroy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmorParticleDestroy : MonoBehaviour
+{
+    public static ArmorParticleDestroy Instance;
+    public ParticleSystem ps;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (ArmorParticleDestroy.Instance == null)
+        {
+            ArmorParticleDestroy.Instance = this;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play(Vector3 pos)
+    {
+        ParticleSystem newps = GameObject.Instantiate(ps, pos, Quaternion.identity);
+        newps.Play();
+    }
+}
